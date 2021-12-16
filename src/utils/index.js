8c5e9bf95ec7927a92047ff08932ba71d7d0a9f8
Fapalz <blacesmot@gmail.com>
2021-12-16 10:57:15 +0500

    feat: add isElementVisible method

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,8 @@
+/**
+ * Returns a random string of given length
+ * @param {number} i Characters
+ * @returns {string}
+ */
 const randomString = (i) => {
   let rnd = ''
   while (rnd.length < i) rnd += Math.random().toString(36).substring(2)
@@ -26,13 +31,55 @@ const throttle = (fn, threshhold = 250, scope) => {
   }
 }
 
-const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
+/**
+ * Return capitalize string
+ * @param {string} string
+ * @returns {string}
+ */
+const capitalize = (string) => {
+  if (typeof string !== 'string') return ''
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-const isDomElement = (obj) => {
-  return !!(obj && obj.nodeType === 1)
+/**
+ * Returns the element is HTMLElement or not
+ * @param {HTMLElement} el
+ * @returns {Boolean}
+ */
+const isDomElement = (el) => {
+  return !!(el && el.nodeType === 1)
 }
 
-export { randomString, throttle, capitalize, isDomElement }
+/**
+ * Returns the element is visible on the viewport or not
+ * @param {HTMLElement} el
+ * @returns {Boolean}
+ */
+const isElementVisible = (el) => {
+  const rect = el.getBoundingClientRect()
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight
+  const efp = (x, y) => {
+    return document.elementFromPoint(x, y)
+  }
+
+  // Return false if it's not in the viewport
+  if (
+    rect.right < 0 ||
+    rect.bottom < 0 ||
+    rect.left > windowWidth ||
+    rect.top > windowHeight
+  )
+    return false
+
+  // Return true if any of its four corners are visible
+  return (
+    el.contains(efp(rect.left, rect.top)) ||
+    el.contains(efp(rect.right, rect.top)) ||
+    el.contains(efp(rect.right, rect.bottom)) ||
+    el.contains(efp(rect.left, rect.bottom))
+  )
+}
+
+export { randomString, throttle, capitalize, isDomElement, isElementVisible }
